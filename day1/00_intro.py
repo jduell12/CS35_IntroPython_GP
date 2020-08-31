0861c9affdb54ca0b3592a79e9bbaef77a789bf5
Jessica <duellje@oregonstate.edu>
2020-08-31 13:04:59 -0400

    completed guided project exercises

--- day1/00_intro.py
+++ day1/00_intro.py
@@ -3,12 +3,15 @@
 
 # lets print a string
 # print("Hello, CS35!", "Some other text", "and theres more...")
+# print('Hello, CS35!', "Some other text", "And there's more")
+# print('Hello, world!')
+
 # variables
 # label = value
 # let const var (js)
 # int bool short (c)
 # first_name = "Tom"
-# # print("Hello CS35 and" , first_name)
+# print("Hello CS35 and", first_name)
 # num = 23.87
 
 # # f strings
@@ -26,13 +29,15 @@
 # collections
 
 # create an empty list? Array
+# my_list = []
 
-
-# print(my_list)
+# print(f'my_list is {my_list}')
 
 # create a list with numbers 1, 2, 3, 4, 5
+# lst1 = [1, 2, 3, 4, 5]
 
-# add an element 24 to lst1
+# # add an element 24 to lst1
+# lst1.append(24)
 
 # print(lst1)
 
@@ -41,10 +46,14 @@
 
 
 # loop over the list using a for loop
-
+# for i in range(len(lst1)):
+#     print(lst1[i])
 
 # while loop
-
+# count = 0
+# while count < len(lst1):
+#     print(lst1[count])
+#     count += 1
 
 # List Comprehensions
 
@@ -52,11 +61,19 @@
 numbers = [1, 2, 3, 4]
 squares = []
 
+# using for loop to add squares of all values in numbers
+for i in range(len(numbers)):
+    squares.append(numbers[i]**2)
+
 print(numbers)
 print(squares)
 # Filtering with a list comprehension
 evens = []
 
+# using for loop and if statement to get even numbers
+for i in range(len(numbers)):
+    if numbers[i] % 2 == 0:
+        evens.append(numbers[i])
 
 # create a new list of even numbers using the values of the numbers list as inputs
 
@@ -65,8 +82,13 @@ print(evens)
 # create a new list containing only the names that start with 's' make sure they are capitalized (regardless of their original case)
 names = ["Sarah", "jorge", "sam", "frank", "bob", "sandy"]
 s_names = []
-print(s_names)
 
+for i in range(len(names)):
+    name = names[i].capitalize()
+    if name[0] == 'S':
+        s_names.append(name)
+
+print(s_names)
 
 # Dictionaries
 
